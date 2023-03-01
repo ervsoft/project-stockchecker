4f394febceb5d358948fd8001baa1c7a3570b3ba
macos <ag81@aof.anadolu.edu.tr>
2023-03-01 13:10:57 +0300

    dsag

--- routes/api.js
+++ routes/api.js
@@ -8,7 +8,6 @@ module.exports = function (app) {
       const stocks = req.query.stock;
       const like = req.query.like;
       console.log(stocks, like);
-
       const requests = Array.isArray(stocks) ? stocks.map(stock => {
         return axios.get(`https://stock-price-checker-proxy.freecodecamp.rocks/v1/stock/${stock}/quote`);
       }) : [axios.get(`https://stock-price-checker-proxy.freecodecamp.rocks/v1/stock/${stocks}/quote`)];
@@ -26,7 +25,7 @@ module.exports = function (app) {
           //   stockData[0].rel_likes = stockData[0].likes - stockData[1].likes;
           //   stockData[1].rel_likes = stockData[1].likes - stockData[0].likes;
           // }
-          res.json({ stockData });
+          res.json({ stockData: stockData });
         })
         .catch(error => {
           console.log(error);
