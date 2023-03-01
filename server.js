b6d19915574ecba3ea1a9dc5fc39138d1c61724a
macos <ag81@aof.anadolu.edu.tr>
2023-03-01 08:58:25 +0300

    dasdas

--- server.js
+++ server.js
@@ -5,9 +5,12 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const helmet = require('helmet');
 require('dotenv').config();
+const URI = process.env.MONGO_URI;
+const mongoose = require('mongoose');
 const apiRoutes = require('./routes/api.js');
 const fccTestingRoutes = require('./routes/fcctesting.js');
 const runner = require('./test-runner');
+const databaseName = 'stockPriceChecker';
 const app = express();
 app.use(helmet());
 app.use(helmet.contentSecurityPolicy({
@@ -16,7 +19,36 @@ app.use(helmet.contentSecurityPolicy({
     styleSrc: ["'self'"]
   }
 }))
+app.set('trust proxy', true);
+mongoose.Promise = global.Promise;
+
+const start = async () => {
+  await mongoose.connect(URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true
+  });
+}
+
+start();
+
+const db = mongoose.connection
+db.on('error', err => { console.error(err) })
+db.once('open', () => {
+  console.log('Connected to' + databaseName)
+})
+
+process.on('SIGNIT', () => {
+  db.close(() => {
+    console.log(`Closing connection to ${databaseName}`)
+    process.exit(0)
+  })
+})
+
 app.use('/public', express.static(process.cwd() + '/public'));
+
+app.use(cors({ origin: '*' })); //For FCC testing purposes only
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -41,16 +73,17 @@ app.use(function (req, res, next) {
 });
 
 //Start our server and tests!
-const listener = app.listen(process.env.PORT || 3000, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
+app.listen(process.env.PORT || 3000, function () {
+  console.log("Listening on port " + process.env.PORT);
   if (process.env.NODE_ENV === 'test') {
     console.log('Running Tests...');
     setTimeout(function () {
       try {
         runner.run();
       } catch (e) {
+        var error = e;
         console.log('Tests are not valid:');
-        console.error(e);
+        console.log(error);
       }
     }, 3500);
   }
